0a8cc5465c0000b24bd67966dffc783e7f4ad08a
Alfie Tompsett <at05266340@priestley.ac.uk>
2024-10-18 15:43:31 +0100

    Made the code

--- Insertion sort/Insertion_sort.py
+++ Insertion sort/Insertion_sort.py
@@ -1,1 +1,30 @@
+import random
+def randomlist():
+    nums = []
+    for i in range(0,100000):
+        n = random.randint(1,99)
+        nums.append(n)
+    return (nums)
+
+
+def insertionsort():
+    nlist = randomlist()
+    
+    for i in range(1, len(nlist)):
+        
+        currentval = nlist[i]
+        
+        
+        position = i
+        
+        while position > 0 and nlist[position - 1] > currentval:
+            
+            nlist[position] = nlist [position - 1]
+            position -= 1
+            
+            nlist[position] = currentval
+            
+    return nlist
+print(insertionsort())
+
 
